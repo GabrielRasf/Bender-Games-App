545aa5c50db095bf263b2e513d0bdfece31a5fda
Gabriel R <gabrielrasf@hotmail.com>
2025-07-10 20:05:46 -0300

    atualização

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.app.bebedeiragames"
         minSdk = 23
         targetSdk = 35
-        versionCode = 21
-        versionName = "1.2.1"
+        versionCode = 23
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
